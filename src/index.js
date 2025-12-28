7c94680af05331d08e3764e3a0b0f710e9f79e66
Rahmahdaniah Fitri <11231084@student.itk.ac.id>
2025-12-29 07:39:24 +0800

    test-db endpoint cui

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ dotenv.config(); // ✅ HARUS PALING ATAS SEBELUM DB
 import express from "express";
 import cors from "cors";
 import pool from "./config/db.js";
+// import mysql from "mysql2/promise";
 
 import emotionRoutes from "./routes/emotion.js";
 import userRoutes from "./routes/user.js";
@@ -27,6 +28,23 @@ app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
+app.get("/test-db", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() as now");
+    res.json({
+      status: "OK",
+      db_time: rows[0].now,
+    });
+  } catch (err) {
+    console.error("DB TEST ERROR:", err);
+    res.status(500).json({
+      status: "ERROR",
+      message: err.message,
+    });
+  }
+});
+
+
 app.use("/api/user", userRoutes);
 app.use("/api/predict", emotionRoutes);
 
@@ -34,7 +52,9 @@ app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
